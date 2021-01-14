b3b9a34ed0c9a6433bd72624526b911f3fccf0d4
Fran99 <francogarzon99@gmail.com>
2021-01-14 11:13:49 -0300

    fix reportWebVitals.ts

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,5 +1,5 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { ReportHandler } from 'web-vitals';
-
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
